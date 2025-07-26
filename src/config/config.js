ceadb2d558899e2f74418b8b0ac59aa2a280684f
suyogshejal2004 <161924832+suyogshejal2004@users.noreply.github.com>
2025-07-26 12:41:46 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,10 @@ import "dotenv/config.js";
 import fastifySession from "@fastify/session";
 import ConnectMongoDBSession from "connect-mongodb-session";
 import { Admin } from "../models/index.js";
+
 export const PORT = process.env.PORT || 3000;
 export const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD;
+
 const MongoDBStore = ConnectMongoDBSession(fastifySession);
 
 export const sessionStore = new MongoDBStore({
@@ -16,28 +18,12 @@ sessionStore.on("error", (error) => {
 });
 
 export const authenticate = async (email, password) => {
-  //creating admin frist time
-
- /* if (email && password) {
-    if ( email === "suyogshejal2004@gmail.com" && password === "Suyog@2004") {
-      return Promise.resolve({ email: email, password: password }); // you can handle invalid password here
-    } else {
-      return null; // you can handle invalid password here
-    }
-  } */
-// ok created testing 
-  //UNcomment this when creadet admin panel manually in databse
   if (email && password) {
     const user = await Admin.findOne({ email });
-    if (!user) {
-      return null; // you can handle invalid user here
-    }
+    if (!user) return null;
     if (user.password === password) {
-      return Promise.resolve({ email: email, password: password }); // you can handle invalid password here
-    } else {
-      return null; // you can handle invalid password here
+      return Promise.resolve({ email: user.email, role: user.role });
     }
-  } 
-
+  }
   return null;
 };
